96c819b957fb3e91f973e4f2da39b1fd491aac64
Ben Dent <Ben@Bens-MacBook-Pro.local>
2016-12-07 11:23:26 +0000

    Added Alien going to random location

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import turtle
+import random
 
+turtle.register_shape('Invader.gif')
 
+class Invaders(turtle.Turtle):
+    
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.pu()
+        self.goto(random.randint(-100, 100), random.randint(100,200))
+        self.active = True
+        
+        
+Invaders()
+        
+    
